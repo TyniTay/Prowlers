89ae6e1ca9f7b5bc6101aaf50efd5dc5fb9dda3d
julescbs <20586059+julescbs@users.noreply.github.com>
2020-11-24 17:58:43 -0600

    working resources page

--- src/Components/Pages/Resources/Resources.js
+++ src/Components/Pages/Resources/Resources.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Resources.css'
 
 const Resources = props => {
     return(
-        <div>
-            This will be the resources page
+        <div className = "resources">
+            <a className = "resource" 
+                style={{ background: '#edc7c7' }}
+                href="https://bannerxe.lmu.edu/StudentRegistrationSsb/ssb/registration" 
+                target="_blank">register for classes
+            </a>
+            <a className = "resource" 
+                style={{ background: '#c7d9ed' }}
+                href="https://lmutpg.lmu.edu/C20995_tsa/web/welcome.jsp" 
+                target="_blank">student account center
+            </a> 
+            {/* The one below is just a placeholder until we get more stuff */}
+            <a className = "resource" 
+                href="https://www.youtube.com/watch?v=RVdZs8Vh0O0" 
+                target="_blank">furret walking
+            </a> 
         </div>
     )
 }
