eb5d65d368b4eea5254871669118553921bf192e
julescbs <20586059+julescbs@users.noreply.github.com>
2020-11-28 17:58:07 -0600

    removed project-wide bootstrap

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -18,6 +18,7 @@ function Navbar() {
                     <RiIcons.RiMenuLine onClick={showSidebar}/>
                 </Link>
                 {SearchBar()}
+                <h1>PAGE TITLE</h1>
             </div>
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className='nav-menu-items' onClick={showSidebar}>
