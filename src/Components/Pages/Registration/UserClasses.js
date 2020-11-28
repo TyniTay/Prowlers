db5729b80b6a66e4067db70cc713957501e28f4c
jstukenb <jstukenb@lion.lmu.edu>
2020-11-28 15:13:27 -0800

    pushing beginning of registration

---
+++ src/Components/Pages/Registration/UserClasses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserClasses = props => {
+    console.log("userCLasses", props)
+    return (
+        <div style={{backgroundColor:"lightyellow"}}>
+            These are your classes taht you are enrolled in
+        </div>
+    )
+}
+
+export default UserClasses
