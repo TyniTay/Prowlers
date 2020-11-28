d86982ffd12505c535147b1e923e98befef17ee0
aidan242 <46692414+aidan242@users.noreply.github.com>
2020-11-27 19:33:22 -0800

    added file (pls work)

--- src/Components/Pages/FinancialAid/FinancialAid.js
+++ src/Components/Pages/FinancialAid/FinancialAid.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './FinancialAid.css'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import { RiBarcodeBoxFill } from 'react-icons/ri';
+
+const schoolYears=["2020-2021","2019-2020","2018-2019","2017-2018","2016-2017","2015-2016","2014-2015"]
+const defaultOption=schoolYears[0];
+
+function dropDown(){
+    return(
+        <h2 className='dropDown'>
+        <Dropdown options={schoolYears} onChange={schoolYears._onSelect} value={defaultOption} 
+            placeholder="Select an option" />
+    </h2>
+    )
+}
+
+function docUpload(){
+    return(
+    <a className = "docUpload" 
+        href="https://financialaid.lmu.edu/generalinformation/securedocumentupload/" 
+        target="_blank">Secure Document Upload
+    </a> 
+    )
+}
 
 const FinancialAid = props => {
     return(
-        <div>
-            This is the financial aid page
+        <div className='financePage'>
+            Financial Aid
+            <h1 className='yearSelect'>
+                Financial Aid Status:
+            </h1>
+
+            {dropDown()}
+            {docUpload()}
+
+            <p className='award'>
+                Financial Aid Award:
+                <br/>
+                <br/>
+                <br/>
+                $XXXXXX
+                <a className = "fullaccount" 
+                href="https://banner-web.lmu.edu:8080/pls/pbanr/bwskoacc.P_ViewAcct" 
+                target="_blank">View full account numbers
+                </a> 
+            </p>
+
+            
+            <p className="standing">
+                Academic Standing<br/><br/><br/>
+                Good
+            </p>
+
+            <p className="holds">
+                Holds<br/><br/><br/>No Holds at this time
+            </p>
+            <p className='payment'>
+                Award Payment Schedule
+                <table className="table">
+                <tr> <td> Award   </td> <td> Expected Date </td> <td>      Paid to Date</td> </tr>
+                <tr> <td> X </td> <td> xx/xx/xx </td> <td>X</td>  </tr> 
+                <tr> <td> X </td> <td> xx/xx/xx </td> <td>X</td>  </tr> 
+                <tr> <td> X </td> <td> xx/xx/xx </td> <td>X</td>  </tr> 
+                </table> 
+            </p>
         </div>
     )
 }
 
 export default FinancialAid
+
