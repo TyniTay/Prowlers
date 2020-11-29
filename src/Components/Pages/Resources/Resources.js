0f202ffd1bb51935ea99978d8aa3f6df09766fb2
Taylor <tyynitay@gmail.com>
2020-11-28 20:44:15 -0800

    Buttons

--- src/Components/Pages/Resources/Resources.js
+++ src/Components/Pages/Resources/Resources.js
@@ -14,48 +14,66 @@ return (
     <section className="astyle">
     <text className="column">
         <h3 className="aresource">Test</h3>
-        <h2 className="atitle">PROWL</h2>
-        <p className="atext">Stuff inside the box weeee!</p>
+        <h2 className="atitle">OneCard Account</h2>
+        <p className="atext">Access to your OneCard balances for meal plans, laundry, and more.</p>
+        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+        </a>
     </text>
     </section>
 
     <section className="astyle">
     <text className="column">
         <h3 className="aresource">Test</h3>
-        <h2 className="atitle">PROWL</h2>
-        <p className="atext">Stuff inside the box weeee!</p>
+        <h2 className="atitle">Degree Works</h2>
+        <p className="atext">Check your Degree Audit and check your gradutation status.</p>
+        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+        </a>
     </text>
     </section>
 
     <section className="astyle">
     <text className="column">
         <h3 className="aresource">Test</h3>
-        <h2 className="atitle">PROWL</h2>
-        <p className="atext">Stuff inside the box weeee!</p>
+        <h2 className="atitle">Brightspace</h2>
+        <p className="atext">Keep up with updates for current courses exams, grades, homework, and more.</p>
+        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+        </a>
     </text>
     </section>
 
     <section className="astyle">
     <text className="column">
         <h3 className="aresource">Test</h3>
-        <h2 className="atitle">PROWL</h2>
-        <p className="atext">Stuff inside the box weeee!</p>
+        <h2 className="atitle">LibGuides</h2>
+        <p className="atext">Full list of Databases accessible through the library.</p>
+        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+        </a>
     </text>
     </section>
 
     <section className="astyle">
     <text className="column">
         <h3 className="aresource">Test</h3>
-        <h2 className="atitle">PROWL</h2>
-        <p className="atext">Stuff inside the box weeee!</p>
+        <h2 className="atitle">LMU Park Account</h2>
+        <p className="atext">Update your parking information or pay parking fines.</p>
+        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+        </a>
     </text>
     </section>
 
     <section className="astyle">
     <text className="column">
         <h3 className="aresource">Test</h3>
-        <h2 className="atitle">PROWL</h2>
-        <p className="atext">Stuff inside the box weeee!</p>
+        <h2 className="atitle"> DSS Online Portal </h2>
+        <p className="atext">Apply or manage your accommodations.</p>
+        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+        </a>
     </text>
     </section>
     
