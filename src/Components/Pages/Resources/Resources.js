7c445e16babcb29d76f887a081ce27637dacbc5c
Taylor <tyynitay@gmail.com>
2020-11-28 19:13:17 -0800

    Resources

--- src/Components/Pages/Resources/Resources.js
+++ src/Components/Pages/Resources/Resources.js
@@ -2,25 +2,26 @@ import React from 'react'
 import './Resources.css'
 
 const Resources = props => {
-    return(
-        <div className = "resources">
-            <a className = "resource" 
-                style={{ background: '#edc7c7' }}
-                href="https://bannerxe.lmu.edu/StudentRegistrationSsb/ssb/registration" 
-                target="_blank">register for classes
-            </a>
-            <a className = "resource" 
-                style={{ background: '#c7d9ed' }}
-                href="https://lmutpg.lmu.edu/C20995_tsa/web/welcome.jsp" 
-                target="_blank">student account center
-            </a> 
-            {/* The one below is just a placeholder until we get more stuff */}
-            <a className = "resource" 
-                href="https://www.youtube.com/watch?v=RVdZs8Vh0O0" 
-                target="_blank">furret walking
-            </a> 
-        </div>
-    )
-}
+return (
+<html>
 
+<head>
+    <title>Resources</title>
+</head>
+
+<body>
+<div className="container">
+    <section className="astyle">
+    <text className="column">
+        <h3 className="aresource">Test</h3>
+        <h2 className="atitle">PROWL</h2>
+        <p className="atext">Stuff inside the box weeee!</p>
+    </text>
+    </section>
+</div>
+
+</body>
+</html>
+)
+}
 export default Resources
