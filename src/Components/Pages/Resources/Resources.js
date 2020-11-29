5e8b365c26bd4196cd2b682f95a660bdbde89ffa
julescbs <20586059+julescbs@users.noreply.github.com>
2020-11-29 01:11:13 -0600

    buttons centered

--- src/Components/Pages/Resources/Resources.js
+++ src/Components/Pages/Resources/Resources.js
@@ -15,60 +15,60 @@ return (
     <div className="column">
         <h2 className="atitle">OneCard Account</h2>
         <p className="atext">Access to your OneCard balances for meal plans, laundry, and more.</p>
-        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
-        <button className="button"> View </button>
-        </a>
     </div>
+     <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+    </a>
     </section>
 
     <section className="astyle">
     <div className="column">
         <h2 className="atitle">Degree Works</h2>
         <p className="atext">Check your Degree Audit and check your gradutation status.</p>
-        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
-        <button className="button"> View </button>
-        </a>
     </div>
+     <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+    </a>
     </section>
 
     <section className="astyle">
     <div className="column">
         <h2 className="atitle">Brightspace</h2>
         <p className="atext">Keep up with updates for current courses exams, grades, homework, and more.</p>
-        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
-        <button className="button"> View </button>
-        </a>
     </div>
+    <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+    </a>
     </section>
 
     <section className="astyle">
     <div className="column">
         <h2 className="atitle">LibGuides</h2>
         <p className="atext">Full list of Databases accessible through the library.</p>
-        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
-        <button className="button"> View </button>
-        </a>
     </div>
+    <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+    </a>
     </section>
 
     <section className="astyle">
     <div className="column">
         <h2 className="atitle">LMU Park Account</h2>
         <p className="atext">Update your parking information or pay parking fines.</p>
-        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
-        <button className="button"> View </button>
-        </a>
     </div>
+    <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+    </a>
     </section>
 
     <section className="astyle">
     <div className="column">
         <h2 className="atitle"> DSS Online Portal </h2>
         <p className="atext">Apply or manage your accommodations.</p>
-        <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
-        <button className="button"> View </button>
-        </a>
     </div>
+    <a href="https://lmutpg.lmu.edu/C20995_tsa/web/login.jsp" target="_blank"> 
+        <button className="button"> View </button>
+    </a>
     </section>
     
 </div>
