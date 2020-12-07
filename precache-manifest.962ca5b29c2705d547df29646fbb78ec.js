56f2522fa95c6ed8a1c90bbad0f55a7e32159eb8
Aidan O'Donnell <i.am.aidan@gmail.com>
2020-12-07 07:57:59 -0800

    Updates

---
+++ precache-manifest.962ca5b29c2705d547df29646fbb78ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4536f0d3b59ecf8dc6b0caed9166b4e2",
+    "url": "/Prowlers/index.html"
+  },
+  {
+    "revision": "9229b04ab59ed9ba5e53",
+    "url": "/Prowlers/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "3fb87d9db454d51a4e94",
+    "url": "/Prowlers/static/css/main.b8670de4.chunk.css"
+  },
+  {
+    "revision": "9229b04ab59ed9ba5e53",
+    "url": "/Prowlers/static/js/2.c095e56a.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/Prowlers/static/js/2.c095e56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb87d9db454d51a4e94",
+    "url": "/Prowlers/static/js/main.3ed99989.chunk.js"
+  },
+  {
+    "revision": "b25fee6f94236c2660ca",
+    "url": "/Prowlers/static/js/runtime-main.aa757cfb.js"
+  },
+  {
+    "revision": "7aa552e262d8b93f7c1d052717a3a784",
+    "url": "/Prowlers/static/media/calendar.7aa552e2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d31fdc84620b356343ba379131c76c18.js"
+  "/Prowlers/precache-manifest.962ca5b29c2705d547df29646fbb78ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Prowlers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
